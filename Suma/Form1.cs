580de31c302aa93955693da85abd7b193072a7cc
sindy <sindy@SINDY_ARCE>
2022-09-17 23:12:46 -0600

    Agregar archivos de proyecto.

---
+++ Suma/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Suma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            //Crear objeto
+            clSuma suma = new clSuma(int.Parse(txtNum1.Text), int.Parse(txtNum2.Text));
+            textBox3.Text = suma.suma().ToString();
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            txtNum1.Clear();
+            txtNum2.Clear();
+            textBox3.Clear();
+        }
+    }
+}
---
+++ Suma/clSuma.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma
+{
+    class clSuma
+    {
+        //Atributos
+        private int num1;
+        private int num2;
+
+        //Constructor(Es el que dirige)
+        public clSuma(int num1, int num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+
+        //Método
+        public int suma()
+        {
+            return (num1 + num2);
+        }
+    }
+}
